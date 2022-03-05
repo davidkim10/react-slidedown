e6f13863b0cc85890ca2146f3844d7446d1dab33
David K <56006438+davidkim10@users.noreply.github.com>
2022-03-04 22:53:27 -0500

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,1 +1,27 @@
+type ClassNameArray = (string | boolean)[];
 
+export const getClassNames = (classNames: ClassNameArray): string => {
+  return classNames.filter(Boolean).join(" ");
+};
+
+
+// Not used, but a nice util if you need to create an object of classNames 
+// vs using getClassNames multiple times :)
+type ClassNamesObj = {
+  [key: string]: ClassNameArray;
+};
+
+export const createClassNames = <T extends ClassNamesObj>(obj: T) => {
+  type TypedResult = { [key in keyof T]: string };
+
+  const result = Object.keys(obj).reduce((acc, key: string) => {
+    const classNameArray = obj[key];
+    const filteredClassNames = getClassNames(classNameArray);
+    return {
+      ...acc,
+      [key]: filteredClassNames,
+    };
+  }, {} as TypedResult);
+
+  return result;
+};
